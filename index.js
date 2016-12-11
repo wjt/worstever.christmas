708c40976c48dd75d45b3b0d1bd7d11f250cf12c
Will Thompson <will@willthompson.co.uk>
2016-12-11 22:22:36 +0000

    somewhat extraneous

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
 ;(function() {
     'use strict';
 
-    var imgs = document.querySelectorAll('img');
-    var n = imgs.length;
+    var urls = [
+        'beard-hair-redistribution.png',
+        'millerdl_bday.jpg',
+        'millerdl_kwoo.png',
+        'millerdl.png',
+        'millerdl_question.png',
+        'millerdl_suit.png',
+        'millerdl_trotsky.png',
+        'millerdl_viking.jpg',
+        'president-millerdl.png',
+        'santa.png'
+    ];
+
+    var n = urls.length;
     var i = Math.floor(Math.random() * n);
-    var img = imgs[i];
+    var url = 'img/' + urls[i];
 
-    img.classList.add('picked');
+    var img = document.createElement('img');
+    img.onload = function() {
+        // Match the top-left pixel's background colour
+        var canvas = document.createElement('canvas');
+        var ctx = canvas.getContext('2d');
+        ctx.drawImage(img, 0, 0);
+        var data = ctx.getImageData(0, 0, 1, 1);
+        var r = data.data[0];
+        var g = data.data[1];
+        var b = data.data[2];
+        var a = data.data[3];  // though alpha is really over-egging the pudding
+        var rgba = 'rgba(' + [r, g, b, (a / 255).toFixed(3)].join(',') + ')';
+        document.body.style.backgroundColor = rgba;
 
-    var canvas = document.createElement('canvas');
-    var ctx = canvas.getContext('2d');
-    ctx.drawImage(img, 0, 0);
-    var data = ctx.getImageData(0, 0, 1, 1);
-    /* TODO: doesn't work, I think the jpeg loads async */
-    var r = data.data[0];
-    var g = data.data[1];
-    var b = data.data[2];
-    var a = data.data[3];
-    var rgba = [r, g, b, (a / 255).toFixed(3)].join(',');
-    console.log(rgba);
+        // Ensure a sensible height
+        var maxHeight = 350;
+        if (img.height > maxHeight) {
+            img.style.height = maxHeight.toFixed(0) + 'px';
+            img.style.width = 'auto';
+        }
 
-    document.body.appendChild(canvas);
+        document.body.appendChild(img);
+    };
+    img.onerror = function(e) {
+        console.error("extremely unfortunate", i, url, e);
+    };
+    img.src = url;
 }());
